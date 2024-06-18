6f177ac2abf0e976eab032c2e12d9ac01f70af00
Oleksandr Perlov <oleksandrperlov@gmail.com>
2024-06-18 19:09:11 +0300

    add svg icons

---
+++ public/icons/index.ts
@@ -0,0 +1,18 @@
+import Instagram from "./instagram.svg";
+import Mail from "./mail.svg";
+import Youtube from "./youtube.svg";
+import Facebook from "./facebook.svg";
+
+// export type IconsType = {
+//     [key:string]: SVGSVGElement
+// }
+
+const  icons = {
+  'instagram': Instagram,
+  'mail': Mail,
+  'youtube': Youtube,
+  'facebook': Facebook,
+};
+
+
+export default icons
