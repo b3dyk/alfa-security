e2ca12c895cc8d5878bf1c3f9979e43ad0e90f5c
Oleksandr Perlov <oleksandrperlov@gmail.com>
2024-07-14 14:42:17 +0300

    manage icons and images

--- public/icons/index.ts
+++ public/icons/index.ts
@@ -2,17 +2,32 @@ import Instagram from "./instagram.svg";
 import Mail from "./mail.svg";
 import Youtube from "./youtube.svg";
 import Facebook from "./facebook.svg";
+import HeroPolygon from "./hero-polygon.svg";
+import BenefitsPolygon from "./benefits-polygon.svg";
+import Call from "./call.svg";
+import Camera from "./camera.svg";
+import Headset from "./headset.svg";
+import Time from "./time.svg";
+import Tool from "./tool.svg";
+import Arrow from "./arrow.svg";
 
-// export type IconsType = {
-//     [key:string]: SVGSVGElement
-// }
-
-const  icons = {
-  'instagram': Instagram,
-  'mail': Mail,
-  'youtube': Youtube,
-  'facebook': Facebook,
+export type IconsType = {
+  [key: string]: SVGSVGElement;
 };
 
+const icons = {
+  instagram: Instagram,
+  mail: Mail,
+  youtube: Youtube,
+  facebook: Facebook,
+  heroPolygon: HeroPolygon,
+  benefitsPolygon: BenefitsPolygon,
+  call: Call,
+  camera: Camera,
+  headset: Headset,
+  time: Time,
+  tool: Tool,
+  arrow: Arrow,
+};
 
-export default icons
+export default icons;
