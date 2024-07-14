4d4afdaf811f6bc01d5428130766ae4c21138f5b
Oleksandr Perlov <oleksandrperlov@gmail.com>
2024-07-14 14:45:06 +0300

    Update socials.ts

--- src/helpers/socials.ts
+++ src/helpers/socials.ts
@@ -1,19 +1,32 @@
-import Instagram from "../../public/icons/instagram.svg";
-import Mail from "../../public/icons/mail.svg";
-import Youtube from "../../public/icons/youtube.svg";
-import Facebook from "../../public/icons/facebook.svg";
+import icons from "../../public/icons";
 
-// export type SocialType = {
-//   id: number;
-//   name: string;
-//   component: SVGProps<SVGSVGElement>;
-// };
+export type SocialType = {
+  id: number;
+  name: keyof typeof icons;
+  href: string;
+};
 
-// export type SocialsType = SocialType[];
+export type SocialsType = SocialType[];
 
-export const SOCIALS = [
-  { id: 1, name: "instagram", component: Instagram, href: "/instagram.com" },
-  { id: 2, name: "mail", component: Mail, href: "mailto:instagram.com" },
-  { id: 3, name: "youtube", component: Youtube, href: "/youtube.com" },
-  { id: 4, name: "facebook", component: Facebook, href: "/facebook.com" },
+export const SOCIALS: SocialsType = [
+  {
+    id: 1,
+    name: "instagram",
+    href: "https://instagram.com",
+  },
+  {
+    id: 2,
+    name: "mail",
+    href: "mailto:instagram.com",
+  },
+  {
+    id: 3,
+    name: "youtube",
+    href: "https://youtube.com",
+  },
+  {
+    id: 4,
+    name: "facebook",
+    href: "https://facebook.com",
+  },
 ];
