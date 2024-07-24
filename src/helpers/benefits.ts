7441abbb1d3545dbe31a73ea907341add229f54f
Oleksandr Perlov <oleksandrperlov@gmail.com>
2024-07-24 17:37:00 +0300

    update benefits

--- src/helpers/benefits.ts
+++ src/helpers/benefits.ts
@@ -1,16 +1,21 @@
 export const BENEFITS = [
   {
     id: 1,
-    title: { number: "5+", text: "РОКІВ" },
+    title: { number: "10+", text: "РОКІВ" },
     subtitle: "СТОЇМО НА ЗАХИСТІ ВАШОЇ БЕЗПЕКИ",
   },
   {
     id: 2,
+    title: { number: "35", text: "РОКІВ" },
+    subtitle: "ДОСВІД НАШОЇ КОМАНДИ",
+  },
+  {
+    id: 3,
     title: { number: "24/7", text: "" },
     subtitle: "ЦІЛОДОБОВО ЗДІЙСНЮЄМО МОНІТОРИНГ ТА РЕАГУВАННЯ",
   },
   {
-    id: 3,
+    id: 4,
     title: { number: "5", text: "ХВИЛИН" },
     subtitle: "ЧАС РЕАГУВАННЯ НА ВИКЛИК",
   },
