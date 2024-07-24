8547de0cf9db68ad5b7f21cbdd0b695cf7520056
Oleksandr Perlov <oleksandrperlov@gmail.com>
2024-07-24 17:35:27 +0300

    add Footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localfont from "next/font/local";
 import "./globals.css";
 import Header from "@/components/Header/Header";
+import Footer from "@/components/Footer/Footer";
 
 const montserrat = localfont({
   src: "../../public/fonts/montserrat-variablefont.woff2",
@@ -22,6 +23,7 @@ export default function RootLayout({
       <body className={montserrat.className}>
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,56 @@
+import Image from "next/image";
+import Link from "next/link";
+import PhonesList from "../PhonesList/PhonesList";
+import css from "./Footer.module.css";
+import ROUTES from "@/helpers/routes";
+import Button from "../Button/Button";
+
+export default function Footer() {
+  return (
+    <footer className={css.footer}>
+      <div className={css.container}>
+        <div className={css.contacts}>
+          <Image
+            src={"/images/logo-alfa-horizontal.png"}
+            alt="logo"
+            width={200}
+            height={100}
+          />
+          <address className={css.address}>
+            <span>03187, м. Київ</span>
+            <span>вул. Академіка Заболотного, 46, офіс 5</span>
+          </address>
+          <PhonesList type="full" />
+          <a className={css.mail} href="mailto:ohorona.24.7@gmail.com">
+            ohorona.24.7@gmail.com
+          </a>
+        </div>
+        <div className={css.info}>
+          <ul className={css.mainList}>
+            {ROUTES.map(({ id, name, path, scalable }) => (
+              <li key={id}>
+                <Link className={css.routeLink} href={path}>
+                  {name}
+                </Link>
+                {scalable.length > 0 && (
+                  <ul className={css.embededList}>
+                    {scalable.map(({ id, text }) => (
+                      <li key={id}>{text}</li>
+                    ))}
+                  </ul>
+                )}
+              </li>
+            ))}
+          </ul>
+          <Button type="button" className={css.footerBtn}>
+            Подати заявку
+          </Button>
+        </div>
+      </div>
+      <div className={css.copyright}>
+        <span>&copy;Alfa Security 2024. All rights reserved.</span>
+        <span>Developed by Oleksandr Perlov and Artur Yushkov</span>
+      </div>
+    </footer>
+  );
+}
