44cc9afa0e796286333971a543b4ca097a0a9dd5
Oleksandr Perlov <oleksandrperlov@gmail.com>
2024-09-24 19:27:33 +0300

    update useResize

--- src/hooks/useResize.ts
+++ src/hooks/useResize.ts
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import useThrottle from "./useThrottle";
 
 const SCREEN = {
   TABLET: 764,
@@ -6,27 +7,55 @@ const SCREEN = {
 };
 
 export const useResize = () => {
-  const [width, setWidth] = useState<number | undefined>(undefined);
+  const getWindowSize = (): { width: number; height: number } => {
+    if (typeof window === "undefined") return { width: 0, height: 0 };
+
+    const { innerWidth: width, innerHeight: height } = window;
+    return { width, height };
+  };
+
+  const [windowSize, setWindowSize] = useState(getWindowSize());
+
+  const handleWindowResize = () => setWindowSize(getWindowSize());
+  const throttledHandleWindowResize = useThrottle(handleWindowResize, 1000);
 
   useEffect(() => {
-    if (typeof window !== "undefined") {
-      const handleResize = (event: UIEvent) => {
-        if (event.target instanceof Window) {
-          setWidth(event.target.innerWidth);
-        }
-      };
+    if (typeof window === "undefined") return;
 
-      window.addEventListener("resize", handleResize);
-      return () => {
-        window.removeEventListener("resize", handleResize);
-      };
-    }
-  }, []);
+    window.addEventListener("resize", throttledHandleWindowResize);
 
+    return () =>
+      window.removeEventListener("resize", throttledHandleWindowResize);
+  }, [throttledHandleWindowResize]);
+
+  //==============================================================================================
+  // const [width, setWidth] = useState<number>(window.innerWidth);
+
+  // useEffect(
+  //   () => {
+  //     // if (typeof window !== "undefined") {
+  //     //   const handleResize = (event: UIEvent) => {
+  //     //     if (event.target instanceof Window) {
+  //     //       setWidth(event.target.innerWidth);
+  //     //     }
+  //     //   };
+
+  //     //   window.addEventListener("resize", handleResize);
+  //     //   return () => {
+  //     //     window.removeEventListener("resize", handleResize);
+  //     //   };
+  //     // }
+
+  //   },
+  //   [
+  //     width
+  //   ]
+  // );
+  //=================================================================================================
   return {
-    width,
-    isScreenMobile: width && width < SCREEN.TABLET,
-    isScreenTablet: width && width >= SCREEN.TABLET,
-    isScreenDesktop: width && width >= SCREEN.DESKTOP,
+    windowSize,
+    isScreenMobile: windowSize.width < SCREEN.TABLET,
+    isScreenTablet: windowSize.width >= SCREEN.TABLET,
+    isScreenDesktop: windowSize.width >= SCREEN.DESKTOP,
   };
 };
