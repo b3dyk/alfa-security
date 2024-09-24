971caa86e87d3117c8150c365f805aed9ed2e144
Oleksandr Perlov <oleksandrperlov@gmail.com>
2024-09-24 19:26:05 +0300

    move CallbackForm to common components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 import Benefits from "./components/Benefits/Benefits";
 import Hero from "./components/Hero/Hero";
 import About from "./components/About/About";
-import CallbackForm from "./components/CallbackForm/CallbackForm";
 import css from "./page.module.css";
 import HeroNew from "./components/HeroNew/HeroNew";
+import CallbackForm from "@/components/CallbackForm/CallbackForm";
 
 export default function Home() {
   return (
