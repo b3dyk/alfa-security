a7424a0857998486ee82e19e926d8933abc0ca4c
Oleksandr Perlov <oleksandrperlov@gmail.com>
2024-10-21 20:14:02 +0300

    add keen-slider

--- src/app/components/Hero/components/AuctionCards/AuctionCards.tsx
+++ src/app/components/Hero/components/AuctionCards/AuctionCards.tsx
@@ -4,24 +4,36 @@ import Image from "next/image";
 import css from "./AuctionCards.module.css";
 import { AUCTION_CARDS } from "@/helpers/auctionCards";
 import { Icon } from "@/components/Icon/Icon";
-import { useCarousel } from "./hooks/useCarousel";
+
+import { useKeenSlider } from "keen-slider/react";
+import "keen-slider/keen-slider.min.css";
+import { useResize } from "@/hooks/useResize";
 
 export default function AuctionCards() {
-  const { next, prev, isDisabled, carouselStyles } = useCarousel();
+  const { isScreenDesktop } = useResize();
+
+  const [sliderRef, slider] = useKeenSlider<HTMLUListElement>({
+    loop: true,
+    slides: {
+      perView: isScreenDesktop ? 2 : 1,
+      spacing: 20,
+      origin: isScreenDesktop ? "auto" : "center",
+    },
+  });
+
   return (
     <div className={css.auctionSlider}>
       <button
         type="button"
         className={css.buttonLeft}
-        disabled={isDisabled}
-        onClick={prev}
+        onClick={() => slider.current?.prev()}
       >
         <Icon glyph="arrow" />
       </button>
       <div className={css.carouselWrapper}>
-        <ul className={css.cardList} style={carouselStyles}>
+        <ul ref={sliderRef} className={`keen-slider ${css.cardList}`}>
           {AUCTION_CARDS.map(({ id, src, title, description }) => (
-            <li key={id} className={css.cardItem}>
+            <li key={id} className={`keen-slider__slide ${css.cardItem}`}>
               <Image
                 src={src}
                 alt="auction"
@@ -41,8 +53,7 @@ export default function AuctionCards() {
       <button
         type="button"
         className={css.buttonRight}
-        disabled={isDisabled}
-        onClick={next}
+        onClick={() => slider.current?.next()}
       >
         <Icon glyph="arrow" />
       </button>
