244337c0693686d252e214f3420c5f5ea942c108
Oleksandr Perlov <oleksandrperlov@gmail.com>
2024-11-22 20:26:12 +0200

    add disclaimer

--- src/app/[pageId]/components/DiscountPage/DiscountPage.tsx
+++ src/app/[pageId]/components/DiscountPage/DiscountPage.tsx
@@ -1,3 +1,9 @@
+import css from "./DiscountPage.module.css";
+
 export default function DiscountPage() {
-  return <h1>Discount</h1>;
+  return (
+    <section className={css.section}>
+      <h1 className={css.disclaimer}>Sorry! This page under construction</h1>
+    </section>
+  );
 }
