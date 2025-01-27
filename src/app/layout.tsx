23f01e318e3c218fd727db0c7bb6d55460fca90f
Oleksandr Perlov <oleksandrperlov@gmail.com>
2025-01-27 17:39:08 +0200

    fix vulnerabilities

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import localfont from "next/font/local";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 import Header from "@/components/Header/Header";
 import Footer from "@/components/Footer/Footer";
@@ -24,6 +25,7 @@ export default function RootLayout({
         <Header />
         {children}
         <Footer />
+        <SpeedInsights />
         <div id="portal-root"></div>
       </body>
     </html>
