011b799f33de35111f79f2b60fb858783eb3bf1a
Oleksandr Perlov <oleksandrperlov@gmail.com>
2025-03-01 20:34:20 +0200

    add highlights on page

--- src/app/[pageId]/components/DiscountPage/components/Discounts/Discounts.tsx
+++ src/app/[pageId]/components/DiscountPage/components/Discounts/Discounts.tsx
@@ -4,12 +4,30 @@ import Image from "next/image";
 import Container from "@/components/Container/Container";
 
 export default function Discounts({ scalable }: PageProps) {
+  const highlightWords = (text: string, highlights: string[]) => {
+    return text.split("\n").map((paragraph, i) => (
+      <p key={i}>
+        {paragraph.split("-").map((word, j) => {
+          const isHighlighted = highlights.includes(word);
+          if (isHighlighted) {
+            return (
+              <span key={j} className="highlight">
+                {word}
+              </span>
+            );
+          }
+          return word;
+        })}
+      </p>
+    ));
+  };
+
   return (
     <section className={css.section}>
       <Container>
         <h2 className={css.title}>Акції</h2>
         <ul className={css.list}>
-          {scalable.map(({ id, src, title, text }) => (
+          {scalable.map(({ id, src, title, text, highlights }) => (
             <li key={id} className={css.item}>
               <h3 className={css.cardTitle}>{title}</h3>
               <Image
@@ -19,7 +37,9 @@ export default function Discounts({ scalable }: PageProps) {
                 height={175}
                 className={css.cardImage}
               />
-              <p className={css.cardText}>{text}</p>
+              <div className={css.cardText}>
+                {highlightWords(text!, highlights!)}
+              </div>
             </li>
           ))}
         </ul>
